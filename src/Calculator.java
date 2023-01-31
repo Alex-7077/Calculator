78df7549361dece59cc431c38cced54721078432
Alex-7077 <alex1138002@gmail.com>
2023-01-31 16:15:13 +0300

    last version

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-            double First;
-            double Second;
-            double Result;
-            char Operation;
-            Scanner reader = new Scanner(System.in);
-            Scanner scnr = new Scanner(System.in);
-            // public void pressEnterToContinue();
-            System.out.println("Welcome to Alex's calculator!");
-        while (true)  {
+        double First;
+        double Second;
+        double Result;
+        char Operation;
+        Scanner reader = new Scanner(System.in);
+        Scanner scnr = new Scanner(System.in);
+        // public void pressEnterToContinue();
+        System.out.println("Welcome to Alex's calculator!");
+        while (true) {
             System.out.println(" ...Press (Enter) to continue...");
             try {
                 System.in.read();
@@ -42,32 +42,8 @@ public class Calculator {
 
             }
             System.out.printf("Result: " + " " + First + " " + Operation + " " + Second + " = " + Result);
-            System.out.print("Do yo want to continue ? (Y/n)>");
-            if ("exit".equals(command)) {
-                System.out.println("Good Bye!");
-                break;
 
-            }
-            scnr.close();
         }
-        private static void printTime() {
-            System.out.printf("%1tT\n", new Date());
-        }
-
-        private static void printDate() {
-            System.out.printf("%1tY-%1$tm-%1$td\n", new Date());
-        }
-
-        private static void printListCommand() {
-            System.out.println(
-                    "'help'\tprint list commands;\n" +
-                            "'exit'\texit from programm;\n" +
-                            "'date'\tprint today's date;\n" +
-                            "'time'\tprint current time;");
-        }
-    }
-                  }
     }
 }
-
 
