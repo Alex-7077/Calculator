33350cb9a14d77f48dbf9c4d5ed179a0cb616369
Alex-7077 <alex1138002@gmail.com>
2023-01-31 13:21:10 +0300

    Check

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Calculator {
-    public static void main(String[] args) {
-System.out.println("check");
+        public static void main (String[]args){
+
+            System.out.println("Welcom to Alex's Calculator");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter first value: ");
+            int First = sc.nextInt();
+
+            System.out.print("\"Select operation e.g. \"+;-;/;*\"\"");
+            String Operation = sc.nextLine();
+
+            System.out.print(" ");
+            String OperationN = sc.nextLine();
+
+            System.out.print("Enter second value: ");
+            int Second = sc.nextInt();
+
+            System.out.println("Result: " + First + " " + Operation + OperationN + " " + Second + " " + "=");
+        }
     }
-}
+
+
